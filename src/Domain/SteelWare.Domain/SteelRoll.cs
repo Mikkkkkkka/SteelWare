40ec120eec11ab4a3ccee3173960fcbb69b2dd23
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-14 18:11:23 +0300

    domain: added the SteelRoll entity and the Domain csproj

---
+++ src/Domain/SteelWare.Domain/SteelRoll.cs
@@ -0,0 +1,18 @@
+﻿namespace SteelWare.Domain;
+
+public class SteelRoll
+{
+    public int Id { get; set; }
+
+    public float Length { get; set; }
+
+    public float Weight { get; set; }
+
+    public DateOnly AddedFrom { get; set; }
+
+    public DateOnly AddedTo { get; set; }
+
+    public DateOnly DeletedAt { get; set; }
+
+    public DateOnly DeletedTo { get; set; }
+}
